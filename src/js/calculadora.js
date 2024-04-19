ec4e8b22adb4309045ad74fa60988f9e3a870b66
Raul <raul.cfr112@gmail.com>
2024-04-19 10:55:55 -0400

    Arrumando o arquivo conf do karma.

---
+++ src/js/calculadora.js
@@ -0,0 +1,19 @@
+var Calculadora = {
+    adicionar: function (num1, num2) {
+        return 0;
+    },
+    subtrair: function (num1, num2) {
+        return 0;
+    },
+    dividir: function (num1, num2) {
+        return 0;
+    },
+    multiplicar: function (num1, num2) {
+        return 0;
+    }
+};
+
+if (typeof module !== 'undefined'
+    && typeof module.exports !== 'undefined'){
+    module.exports = Calculadora;
+}
